a717c3b50db33f49926f0c731f996f7b71bfea07
Nicolas Penante <66042022+NicoBarbosa@users.noreply.github.com>
2024-03-28 03:54:04 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do tabnews.com.br, mas no meu caso é para fazer um projeto de ponta a ponta!
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Bom dia, My Princes!</h1>
+  return <h1>Bom dia, My Princes!</h1>;
 }
 
 export default Home;
